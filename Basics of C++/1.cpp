4546b6937447519608ebb48499e1506cc510013f
Mukesh Burdak <mburdak626@gmail.com>
2025-10-01 22:05:05 +0530

    1.cpp

---
+++ Basics of C++/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World!"
+    <<"\n"<<"C++ better than c"
+    <<"\t"<<"Language"
+    <<endl<<"display"
+    <<"\a"<<"control"
+    <<"\r"<<"Message";
+    return 0;
+}
