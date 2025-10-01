9231697d2365c17fd1ec6bf88985df4d3033c060
Mukesh Burdak <mburdak626@gmail.com>
2025-10-01 22:07:36 +0530

    Create 2.cpp

---
+++ Basics of C++/2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+    int C,f;
+    cout<<"Enter the temperature in Celsius: ";
+    cin>>C;
+
+   f = 9*C/5+32;
+   cout<<"Change temperature in fahrenheit is : "<<f;
+
+   return 0;
+}
